a8256743882052eeeae5aa9163c95e190603ac84
Brillante Wang <brillante.wang@gmail.com>
2017-12-31 16:14:57 +0800

    adjust bubble radiuses

--- graph.js
+++ graph.js
@@ -1,13 +1,13 @@
 import * as d3 from 'd3';
 
 const outerWidth = 1400;
-const outerHeight = 640;
+const outerHeight = 650;
 const margin = { left: 220, top: 120, right: 100, bottom: 100 };
 
 const innerWidth = outerWidth - margin.left - margin.right;
 const innerHeight = outerHeight - margin.top - margin.bottom;
-const rMin = 2;
-const rMax = 5;
+const rMin = 4;
+const rMax = 9;
 let xColumn;
 let yColumn;
 const rColumn = "GDP";
